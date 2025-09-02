f434f0802666a35591f83bdc36a34ecb6f6b9d0f
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-09-02 09:29:40 -0400

    Created defined shorthand for class name and used it in the chrono_ext.h extension.

--- embedded/modules/custom/chrono_ext.h
+++ embedded/modules/custom/chrono_ext.h
@@ -1,17 +1,20 @@
 #ifndef CHRONO_EXT_H
 #define CHRONO_EXT_H
 
+#define _CHRONO_TP std::chrono::time_point
+#define _CHRONO_SC_D std::chrono::steady_clock::duration
+
 #include <chrono>
 
 // Chronos Ext Method
-std::chrono::time_point<std::chrono::steady_clock> getChronoTimeAtNow();
+_CHRONO_TP <std::chrono::steady_clock> getChronoTimeAtNow();
 
-std::chrono::steady_clock::duration castChronoTimeToDurationAtEpoch(std::chrono::time_point<std::chrono::steady_clock> chronoTime);
+_CHRONO_SC_D castChronoTimeToDurationAtEpoch(std::chrono::time_point<std::chrono::steady_clock> chronoTime);
 long long castChronoDurationToMillisLongLong(std::chrono::steady_clock::duration chronoTimeAtNow);
 unsigned long castChronoDurationMillisLongLongToUnsignedLong(long long chronoDurationMillisLongLong);
 
 long long castChronoDurationMillisUnsignedLongToLongLong(unsigned long chronoDurationMillisUnsignedLong);
-std::chrono::steady_clock::duration revertDurationMillisLongLongToChronoDuration(long long durationMillisLongLong);
-std::chrono::time_point<std::chrono::steady_clock> revertChronoDurationToChronoTime(std::chrono::steady_clock::duration chronoDurationSinceEpoch);
+_CHRONO_SC_D revertDurationMillisLongLongToChronoDuration(long long durationMillisLongLong);
+_CHRONO_TP <std::chrono::steady_clock> revertChronoDurationToChronoTime(std::chrono::steady_clock::duration chronoDurationSinceEpoch);
 
 #endif
