77515bf9f8773b88dcd90b895bd093eb712f3d69
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-09-30 22:43:09 -0600

    Added new Enum folder. Created the Plant Info Repo.

--- backend/agentic-api/agentic-greenthumb-api/Repos/PlantInfoRepo.cs
+++ backend/agentic-api/agentic-greenthumb-api/Repos/PlantInfoRepo.cs
@@ -1,6 +1,90 @@
-﻿namespace AgenticGreenthumbApi.Repos
+﻿using AgenticGreenthumbApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace AgenticGreenthumbApi.Repos
 {
     public class PlantInfoRepo
     {
+        private readonly PlantInfoContext _context;
+
+        public PlantInfoRepo(PlantInfoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<PlantInfoModel>>> GetPlantInfos()
+        {
+            return await _context.PlantInfos.ToListAsync();
+        }
+
+        public async Task<ActionResult<PlantInfoModel>?> GetPlantInfoModel(long id)
+        {
+            var plantInfoModel = await _context.PlantInfos.FindAsync(id);
+
+            if (plantInfoModel == null)
+            {
+                return null;
+            }
+
+            return plantInfoModel;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<HttpStatusCode>> PutPlantInfoModel(long id, PlantInfoModel plantInfoModel)
+        {
+            if (id != plantInfoModel.Id)
+            {
+                return HttpStatusCode.BadRequest;
+            }
+
+            _context.Entry(plantInfoModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlantInfoModelExists(id))
+                {
+                    return HttpStatusCode.NotFound;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return HttpStatusCode.NoContent;
+        }
+
+        public async Task<ActionResult<PlantInfoModel>> PostPlantInfoModel(PlantInfoModel plantInfoModel)
+        {
+            _context.PlantInfos.Add(plantInfoModel);
+            await _context.SaveChangesAsync();
+
+            return await Task.FromResult(plantInfoModel);
+        }
+
+        public async Task<ActionResult<HttpStatusCode>> DeletePlantInfoModel(long id)
+        {
+            var plantInfoModel = await _context.PlantInfos.FindAsync(id);
+            if (plantInfoModel == null)
+            {
+                return HttpStatusCode.NotFound;
+            }
+
+            _context.PlantInfos.Remove(plantInfoModel);
+            await _context.SaveChangesAsync();
+
+            return HttpStatusCode.NoContent;
+        }
+
+        private bool PlantInfoModelExists(long id)
+        {
+            return _context.PlantInfos.Any(e => e.Id == id);
+        }
     }
 }
