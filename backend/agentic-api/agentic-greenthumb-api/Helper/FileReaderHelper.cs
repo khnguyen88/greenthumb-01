79d4b4ab91cc1632545919c56042485fbd0cdd2b
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-10-14 15:23:09 -0400

    Updated the file reader helper.

--- backend/agentic-api/agentic-greenthumb-api/Helper/FileReaderHelper.cs
+++ backend/agentic-api/agentic-greenthumb-api/Helper/FileReaderHelper.cs
@@ -14,5 +14,31 @@
             Console.WriteLine($"Context document '{filename}' missing.");
             return "";
         }
+
+        public static string GetDomainFile(string filename)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Domain", filename);
+            if (File.Exists(path))
+            {
+                var reader = File.OpenText(path);
+                return reader.ReadToEnd();
+            }
+
+            Console.WriteLine($"Context document '{filename}' missing.");
+            return "";
+        }
+
+        public static string GetModelFile(string filename)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "Models", filename);
+            if (File.Exists(path))
+            {
+                var reader = File.OpenText(path);
+                return reader.ReadToEnd();
+            }
+
+            Console.WriteLine($"Context document '{filename}' missing.");
+            return "";
+        }
     }
 }
