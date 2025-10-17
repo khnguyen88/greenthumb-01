28b27b036d8ce30372467dca6897aadf809f8df9
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-10-17 12:04:27 -0400

    got the color theme finally working

--- frontend/greenthumb-app/src/app/app.config.ts
+++ frontend/greenthumb-app/src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { MyPreset } from './themes/mypresets';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.my-app-dark',
+        },
+      },
+    }),
+  ],
 };
--- frontend/greenthumb-app/tailwind.config.js
+++ frontend/greenthumb-app/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#10b981', // Tailwind emerald.500
+        surface: '#f0fdf4',
+        text: '#ffffff',
+      },
+    },
   },
   plugins: [],
 };
