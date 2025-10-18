9c4dfd19b0451077f6625e63bf4ff504957cf46a
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-10-18 19:51:42 -0400

    figured out how to style primeng components

--- frontend/greenthumb-app/tailwind.config.js
+++ frontend/greenthumb-app/tailwind.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#10b981', // Tailwind emerald.500
-        surface: '#f0fdf4',
-        text: '#ffffff',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
