3941525d1c49c2b28dc86c0b6b3b5787fe4d8f84
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-10-20 11:04:47 -0400

    Figured out the main page route

--- frontend/greenthumb-app/src/app/components/top-menubar/top-menubar.ts
+++ frontend/greenthumb-app/src/app/components/top-menubar/top-menubar.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
 import { MenuItem } from 'primeng/api';
@@ -18,16 +18,40 @@ export class TopMenubar implements OnInit {
   lightDarkMode = signal('Dark');
   buttonLabel = signal(`Toogle to Dark mode!`);
   pIconClass = signal('pi pi-moon');
+  currentRoute = signal('');
+
+  constructor(private router: Router) {
+    this.currentRoute.set(this.router.url);
+  }
 
   ngOnInit(): void {
     this.items = [
       {
         label: 'Home',
-        icon: 'pi pi-home',
+        icon: 'pi pi-chart-line',
+        routerLink: '/home',
+        command: () => {
+          this.checkRoute();
+          this.updateLogStatus();
+        },
       },
       {
-        label: 'Features',
-        icon: 'pi pi-star',
+        label: 'AI Chat',
+        icon: 'pi pi-comments',
+        routerLink: '/chat',
+        command: () => {
+          this.checkRoute();
+          this.updateLogStatus();
+        },
+      },
+      {
+        label: 'Login',
+        icon: 'pi pi-sign-in',
+        routerLink: '/login',
+        command: () => {
+          this.checkRoute();
+          this.updateLogStatus();
+        },
       },
     ];
   }
@@ -40,5 +64,19 @@ export class TopMenubar implements OnInit {
     this.pIconClass.set(this.lightDarkMode() === 'Light' ? 'pi pi-sun' : 'pi pi-moon');
 
     this.buttonLabel.set(`Toogle to ${this.lightDarkMode()} mode!`);
+  }
+
+  checkRoute() {
+    this.currentRoute.set(this.router.url);
+  }
+
+  updateLogStatus() {
+    //REPLACE THIS ONCE WE FIGUREOUT SIGNIN/SIGNOUT IF ANY
+    var boolCheck: number = Math.round(Math.random());
+
+    const item = this.items?.find((i) => i.label === 'Login' || i.label === 'Logout');
+    if (item) {
+      item.label = boolCheck ? 'Logout' : 'Login';
+    }
   }
 }
